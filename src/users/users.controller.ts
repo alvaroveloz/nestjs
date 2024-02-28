a2ade28bfe8a72fd686c1ece669d426ee1ead817
alvaroveloz <alvaro.veloz@gmail.com>
2024-02-28 15:33:32 -0500

    NESTJS - Added basic controller to users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './interfaces/users.interface';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() user: User): Promise<User> {
+    return this.usersService.create(user);
+  }
+
   @Get()
-  findAll(): string {
-    return 'This action returns all users';
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
   }
 }
