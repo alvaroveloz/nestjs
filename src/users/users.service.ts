a352738dfff35387d23423d0caeb484b561de724
alvaroveloz <alvaro.veloz@gmail.com>
2024-03-04 19:14:42 -0500

    NESTJS -  Added find by Id and by email user services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,19 @@ import { Prisma, User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = this.prisma.user.create({ data });
     return user;
+  }
+
+  async findOne(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { id: parseInt(id) } });
+  }
+
+  async findOneByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { email } });
   }
 
   async findAll(): Promise<User[]> {
