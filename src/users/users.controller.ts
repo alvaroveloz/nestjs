fa3d280670ac7966ce7753a54beed86be5957711
alvaroveloz <alvaro.veloz@gmail.com>
2024-03-04 19:15:05 -0500

    NESTJS - Added Post and Get controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,18 +2,37 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '@prisma/client';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { AuthService } from './auth.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post()
-  async createUser(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async createUser(@Body() body: CreateUserDto) {
+    return this.usersService.create(body);
+  }
+
+  @Post('signup')
+  async signup(@Body() body: CreateUserDto) {
+    return this.authService.signup(body.email, body.password);
   }
 
   @Get()
   async findAll(): Promise<User[]> {
     return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(id: string): Promise<User | null> {
+    return this.usersService.findOne(id);
+  }
+
+  @Get('email/:email')
+  async findOneByEmail(email: string): Promise<User | null> {
+    return this.usersService.findOneByEmail(email);
   }
 }
